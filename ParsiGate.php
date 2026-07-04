993379b54e099225d9290c375bd8f0ed9e2da4d0
Saeed Fard <saeedfard9@gmail.com>
2026-07-05 01:23:45 +0330

    Update ParsiGate.php
    Update License

--- ParsiGate.php
+++ ParsiGate.php
@@ -9,8 +9,8 @@
  * License:     MIT
  * Text Domain: parsigate
  * Domain Path: /languages
- * License:     GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * License:     GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
  * Requires at least: 5.6
  * Requires PHP: 7.4
  * Requires Plugins: wp-parsidate
